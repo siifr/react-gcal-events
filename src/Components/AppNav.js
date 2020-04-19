22919994ff982cb629a940bf84f858c8af9b8880
siifr <support@siifr.net>
2020-04-19 03:36:07 +0000

    Opens links in new tab

--- src/Components/AppNav.js
+++ src/Components/AppNav.js
@@ -10,13 +10,13 @@ export default class AppNav extends Component {
       <div>
         <nav className="flex items-center justify-between flex-wrap bg-red-500 p-6">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <MdPersonPin className="mt-1 mx-2 text-3xl " />
-            <span className="font-semibold text-2xl tracking-tight">Connected | From a Distance</span>
+          <MdPersonPin className="mt-1 mx-2 text-3xl " />
+          <a target="_blank" rel="noopener noreferrer" href="/"><span className="font-semibold text-2xl tracking-tight">Connected | From a Distance</span></a>
           </div>
           
           <div className="flex flex-wrap items-center text-center sm:m-auto md:mr-5 text-white">
             
-            <a className="bg-red-400 hover:bg-red-300 font-bold py-2 sm:mt-5 md:m-auto px-4 border-b-4 border-red-600 hover:border-red-400 rounded" href={apiConf.calendarURL}>View Calendar</a>
+            <a target="_blank" rel="noopener noreferrer" className="bg-red-400 hover:bg-red-300 font-bold py-2 sm:mt-5 md:m-auto px-4 border-b-4 border-red-600 hover:border-red-400 rounded" href={apiConf.calendarURL}>View Calendar</a>
            
           </div>
         </nav>
